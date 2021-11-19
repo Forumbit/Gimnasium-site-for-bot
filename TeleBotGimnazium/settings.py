56e4bcc4012cd80058460c321b6f11a698039f95
Forumbit <amirkharisov545@gmail.com>
2021-11-19 10:08:22 +0300

    third commit

--- TeleBotGimnazium/settings.py
+++ TeleBotGimnazium/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '80=ic!=f6(@$@rf*ls$@t=k=f9^lx$sklghlh5#qe0hhj4#1+4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
